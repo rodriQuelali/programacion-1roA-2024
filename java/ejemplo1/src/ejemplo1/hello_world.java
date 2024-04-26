6247d6c4e31b96c3d732143a949656c7542876f3
Rodri Quelali <rodriquelali@gmail.com>
2024-04-26 11:03:02 -0400

    ejemplo 1

---
+++ java/ejemplo1/src/ejemplo1/hello_world.java
@@ -0,0 +1,13 @@
+package ejemplo1;
+
+public class hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//suintaxis de java
+		System.out.print("hello world");
+
+	}
+
+}

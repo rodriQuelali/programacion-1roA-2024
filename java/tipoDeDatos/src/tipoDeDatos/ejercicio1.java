625ce4c1fd63d6a4aac8513da7cc3b3c9e1d6088
Rodri Quelali <rodriquelali@gmail.com>
2024-04-29 11:46:39 -0400

    tipo de datos

---
+++ java/tipoDeDatos/src/tipoDeDatos/ejercicio1.java
@@ -0,0 +1,18 @@
+package tipoDeDatos;
+
+public class ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//suma de dos numeros
+		int A,B,S;
+		
+		A =20;
+		B = 30;
+		
+		S = A +B;
+		System.out.print(S);
+		
+	}
+
+}
---
+++ java/tipoDeDatos/src/tipoDeDatos/tipoDatos.java
@@ -0,0 +1,61 @@
+package tipoDeDatos;
+
+public class tipoDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//tipo de datos
+		//tipo de datos primtivos
+		/*
+		 * byte -- 8bit
+		 * short -- 16bit
+		 * int -- 32bit
+		 * long -- 64bit
+		 * float -- 32bit
+		 * double -- 64bit
+		 * boolean -- ?
+		 * char -- 16bit
+		 */
+		
+		/*
+		 * numerico(entero):
+		 * byte -- 8bit
+		 * short -- 16bit
+		 * int -- 32bit
+		 * long -- 64bit
+		 * */
+		byte nu1 = 10;
+		short nu2 = 10000;
+		int nu3 = 1000000;
+		long nu4 =1000000000L;
+		
+		
+		/*
+		 * numeros con decimales:
+		 * float -- 32bit
+		 * double -- 64bit
+		 * */
+		float des1 = 3.1415f;
+		double des2 = 100000.0000000;
+		
+		/*
+		 * texto:
+		 * char -- 16bit
+		 * String (no permetece a tipo de datos primivos)
+		 * */
+		char iniNombre = 'R';
+		String nombreCompleto = "rodrigo";
+		
+		
+		/*
+		 * logico:
+		 * boolean -- ?
+		 * */
+		boolean activo = true;
+		boolean inactivo = false;
+		
+		//ejemplo:
+		
+	}
+
+}

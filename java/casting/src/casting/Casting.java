162f2e3474d9cc6c07bb90c927cc822b7f14f822
Rodri Quelali <rodriquelali@gmail.com>
2024-05-13 11:48:50 -0400

    casting

---
+++ java/casting/src/casting/Casting.java
@@ -0,0 +1,24 @@
+package casting;
+
+public class Casting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//cating
+		int a = 1, b=3;
+		float c =3.1f;
+		int s = a + (int)c;
+		boolean aa = true;
+		System.out.print(s);
+		//nooooo
+		//int ss = aa+ (int)c;
+		
+		//parseo de datos
+		String nombre = "23";
+		float sss = 2 + Float.parseFloat(nombre);
+		System.out.print(sss);
+		
+		
+	}
+
+}
--- java/operadores03/src/operadores03/Operadores03.java
+++ java/operadores03/src/operadores03/Operadores03.java
@@ -6,7 +6,7 @@ public class Operadores03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int s,r,m,d;
+		/*int s,r,m,d;
 		System.out.print("ingrese valor A: ");
 		int a = new Scanner(System.in).nextInt();
 		System.out.print("ingrese valor B: ");
@@ -15,7 +15,7 @@ public class Operadores03 {
 		s = a + b;
 		r = a - b;
 		m = a * b;
-		d = a / b;
+		d = a / b;*/
 		
 		/*System.out.println("Operaciones: ");
 		System.out.println("SUMA: " + s);
@@ -47,9 +47,29 @@ public class Operadores03 {
 		c=c+1;//c=1
 		c++;//c=2
 		c--; //c = c -1;
-		System.out.print(c);
-		//+= val
+		//System.out.println(c);
 		
+		
+		
+		
+		
+		
+		
+		
+		//res = res + 5; +=, val
+		int res = 0;
+		res += 5;
+		System.out.println(res);
+		//-, *.
+		//res = res -2;
+		res -= 2;
+		System.out.println(res);
+		//res = res * 4;
+		res *= 4;
+		System.out.println(res);
+		//res = res / 4;
+		res /= 4;
+		System.out.println(res);
 	}
 
 }
